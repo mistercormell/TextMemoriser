f542fdc842c129ffd725846a889c76b18065699c
mrcormell <d.cormell@etoncollege.org.uk>
2021-01-03 14:32:44 +0000

    Initial Commit

---
+++ TextMemoriser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextMemoriser
+//
+//  Created by David Cormell on 03/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextMemoriser/TextMemoriserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextMemoriserApp.swift
+//  TextMemoriser
+//
+//  Created by David Cormell on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TextMemoriserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
