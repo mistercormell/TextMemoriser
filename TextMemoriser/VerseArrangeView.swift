925618ec401d58499e3e6f6106b7e2e5955645cc
mrcormell <d.cormell@etoncollege.org.uk>
2021-01-30 15:49:09 +0000

    Finished Refactoring Verse Builder view

--- TextMemoriser/VerseArrangeView.swift
+++ TextMemoriser/VerseArrangeView.swift
@@ -8,53 +8,12 @@
 import SwiftUI
 
 struct VerseArrangeView: View {
-    let columns = [
-        GridItem(.adaptive(minimum: 80))
-    ]
-    
     @State var wordsInVerse = [WordInVerse]()
     @State var verseBeingBuilt = ""
     @EnvironmentObject var vm: StateController
     
     var body: some View {
-        VStack {
-            Text("\(vm.currentReference?.location.display ?? "")")
-                .font(.largeTitle)
-            Divider()
-            Text(verseBeingBuilt)
-            Spacer()
-            Divider()
-            HStack {
-                Button("Check") {
-                    checkAnswer()
-                }
-                Button("Reset") {
-                    vm.wordsToPick = vm.currentReference?.wordsInVerse ?? []
-                    wordsInVerse = []
-                    verseBeingBuilt = ""
-                }
-            }
-            Divider()
-            LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(vm.wordsToPick, id: \.id) { word in
-                    Button(action: {
-                            wordsInVerse.append(word)
-                        verseBeingBuilt = wordsInVerse.map { word in
-                            word.word
-                        }.joined(separator: " ")
-                        vm.wordsToPick.removeAll {
-                            $0.id == word.id
-                        }
-                    }) {
-                        Text(word.word)
-                            .padding(5)
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(5)
-                    }
-                }
-            }
-        }
+        Content(passage: vm.currentReference, check: checkAnswer, reset: reset, pickWord: pickWord, verseBeingBuilt: $verseBeingBuilt, wordsToPick: $vm.wordsToPick)
         .onAppear(perform: loadReference)
         
     }
@@ -64,12 +23,75 @@ struct VerseArrangeView: View {
     }
     
     func checkAnswer() {
+        if verseBeingBuilt == vm.currentReference?.text {
+            print("Correct")
+        } else {
+            print("Incorrect")
+        }
         vm.nextQuestion()
     }
     
+    func reset() {
+        verseBeingBuilt = ""
+        vm.wordsToPick = vm.currentReference?.wordsInVerse ?? []
+    }
+    
+    func pickWord(word: WordInVerse) {
+        wordsInVerse.append(word)
+        verseBeingBuilt = wordsInVerse.map { word in
+            word.word
+        }.joined(separator: " ")
 
+        vm.wordsToPick.removeAll {
+            $0.id == word.id
+        }
+    }
     
-    
+}
+
+extension VerseArrangeView {
+    struct Content: View {
+        let passage: Passage?
+        let check: () -> Void
+        let reset: () -> Void
+        let pickWord: (WordInVerse) -> Void
+        
+        let columns = [
+            GridItem(.adaptive(minimum: 80))
+        ]
+        
+        @Binding var verseBeingBuilt: String
+        @Binding var wordsToPick: [WordInVerse]
+        
+        var body: some View {
+            VStack {
+                Text("\(passage?.location.display ?? "")")
+                    .font(.largeTitle)
+                Divider()
+                Text(verseBeingBuilt)
+                Spacer()
+                Divider()
+                HStack {
+                    Button("Check", action: check)
+                    Button("Reset", action: reset)
+                }
+                Divider()
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(wordsToPick, id: \.id) { word in
+                        Button(action: { pickWord(word) }) {
+                            Text(word.word)
+                                .padding(5)
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(5)
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+    }
 }
 
 struct VerseArrangeView_Previews: PreviewProvider {
